8ad3b4626ae78f3737ed96e75a0e94b3d08ded8e
servicemeshbook <servicemeshbook@gmail.com>
2019-08-29 00:44:46 -0400

    First Commit

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-        fmt.Fprintf(w, "The HOST IP address is: %s\n", os.Getenv("HOST_IP"))
-        fmt.Fprintf(w, "The POD IP  address is: %s\n", os.Getenv("POD_IP"))
-        fmt.Fprintf(w, "The HOST IP address is: %s\n", os.Getenv("POD_NAME"))
-        fmt.Fprintf(w, "The HOST IP address is: %s\n", os.Getenv("POD_NAME_SPACE"))
+        fmt.Fprintf(w, "The HOST IP       is: %s\n", os.Getenv("HOST_IP"))
+        fmt.Fprintf(w, "The POD IP        is: %s\n", os.Getenv("POD_IP"))
+        fmt.Fprintf(w, "The POD NAME      is: %s\n", os.Getenv("POD_NAME"))
+        fmt.Fprintf(w, "The POD NAMESPACE is: %s\n", os.Getenv("POD_NAMESPACE"))
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
