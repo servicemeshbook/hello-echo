d663a6055e528af3df61ba7d852bf36db442d0ce
servicemeshbook <servicemeshbook@gmail.com>
2019-08-29 18:23:11 -0400

    First Commit

--- rest/main.go
+++ rest/main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-    "io/ioutil"
+    "bytes"
     "fmt"
     "net/http"
     "log"
+    "encoding/json"
     "os"
+    "io/ioutil"
 )
-
-func getEnv(key, fallback string) string {
-    value := os.Getenv(key)
-    if len(value) == 0 {
-        return fallback
-    }
-    return value
-}
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -29,23 +23,23 @@ func main() {
         for k, v := range r.Header {
            fmt.Fprintf(w, "%q: %q\n", k, v)
         }
-
         fmt.Fprintf(w, "===============================================\n")
         fmt.Fprintf(w, "Host       = %q\n", r.Host)
         fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
         fmt.Fprintf(w, "===============================================\n")
-        upstreamService := getEnv("UPSTREAM_SERVICE", "http://httpbin.org/headers") 
-        response, err := http.Get(upstreamService)
+
+        jsonData := map[string]string{"POD_IP": os.Getenv("POD_IP") , "POD_NAME": os.Getenv("POD_NAME") }
+        jsonValue, _ := json.Marshal(jsonData)
+        response, err := http.Post("http://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
         if err != nil {
-          fmt.Printf("The response from %s did not come through %s\n", upstreamService, err)
+          fmt.Printf("The response from http://httpbin.org/post did not come through %s\n", err)
         } else {
           data, _ := ioutil.ReadAll(response.Body)
-          fmt.Fprintf(w, "Response starts from %s\n", upstreamService)
+          fmt.Fprintf(w, "Response starts from http://httpbin.org/post\n")
           fmt.Fprintf(w,string(data))
-          fmt.Fprintf(w, "Response ends from %s\n", upstreamService)
-        }
+          fmt.Fprintf(w, "Response ends from http://httpbin.org/post\n")
+        } 
     })
-
 
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
